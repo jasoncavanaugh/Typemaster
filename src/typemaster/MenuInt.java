2778529950b1ba067b3032ffc5bcc02c3f79ebae
Jason Cavanaugh <jascav418@gmail.com>
2021-02-21 09:59:21 -0500

    Adding one image

--- src/typemaster/MenuInt.java
+++
@@ -1,8 +0,0 @@
-package typemaster;
-
-/* This is our Menu Interface. It is implemented by all the different menus in 
- * the program. Its only method, displayContent(), is called to allow us
- * to display the contents of whichever menu we are referring to.*/
-public interface MenuInt {
-	public void displayContent();
-}
--- src/typemaster/Menus.java
+++
@@ -1,6 +0,0 @@
-package typemaster;
-
-public enum Menus {
-	MainMenu, PlayMenu, YourTextsMenu, ProfileMenu, OptionsMenu, PlayScreen,
-	AddCustomText;
-}
